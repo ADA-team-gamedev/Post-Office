7fc92ccc359b8c46dabc336051a6081a7ffdb668
shozy_maken <120984531+ponich112@users.noreply.github.com>
2023-11-27 22:53:34 +0200

    FlashLight
    p.s without bugs

--- Assets/Scripts/Player/FlashLightController.cs
+++ Assets/Scripts/Player/FlashLightController.cs
@@ -5,40 +5,67 @@ using UnityEngine.UI;
 
 public class FlashLightController : MonoBehaviour
 {
-    private bool _turning = false;
-    private int _timesTurnig = 0;
+    [Header("Objects")]
     [SerializeField] private Light _flashlight;
-    [SerializeField] private Slider _batteryEnergyBar;
-    [SerializeField] private GameObject _batteryBar;
-    [SerializeField] private KeyCode _flashLightInteractionKey;
-    private ItemData[] Inventory;
+    [SerializeField] private Transform _hand;
+    [SerializeField] private Slider _batteryEnergyBar; 
+    [SerializeField] private GameObject _energyBar; 
+    
+
+    [Header("Values")]
+    [SerializeField] private int _timesTurnig;
+    [SerializeField][Range(1, 100)] private float _dischargeRate;
+    
+    private bool _turning = false;
+    [SerializeField] private bool _canTurn = false;
+    private bool _charged = true;
+    [SerializeField] private KeyCode _flashLightInteractionKey;      
     private void Start()
-    {
-        _batteryBar = GameObject.Find("BatteryEnergyBar");
-        _batteryEnergyBar = _batteryBar.GetComponent<Slider>();
+    {       
     }
     private void Update()
     {
-        if (_turning)
-            _batteryEnergyBar.value -= Time.deltaTime / 100;
-        if (!_turning && Input.GetKeyDown(_flashLightInteractionKey))
+        ChangeTurnOn();
+        if (_batteryEnergyBar.value <= 0)
         {
-            Debug.Log(0);            
+            _flashlight.enabled = false;
+            _charged = false;
+        }
+        if (_turning && _charged)
+        {
+            _batteryEnergyBar.value -= Time.deltaTime / _dischargeRate;
+        }          
+        if (!_turning && Input.GetKeyDown(_flashLightInteractionKey) && _canTurn && _charged)
+        {                   
             _flashlight.enabled = true;
             _turning = true;
             _timesTurnig++;
-            foreach (Transform Element in _batteryEnergyBar.transform)
-                Element.gameObject.SetActive(true);                   
+            _energyBar.SetActive(true);
         } 
         else if(_timesTurnig != 0 && Input.GetKeyDown(_flashLightInteractionKey))
-        {
-            Debug.Log(1);
+        {          
             _flashlight.enabled = false;
             _turning = false;
             _timesTurnig--;
-            foreach (Transform Element in _batteryEnergyBar.transform)
-                Element.gameObject.SetActive(false);
-
-        }
+            _energyBar.SetActive(false);
+        }                      
     }   
+    private void ChangeTurnOn()
+    {            
+        foreach(Transform Element in _hand)
+        {
+            if (Element.CompareTag("FlashLight"))
+            {
+                _canTurn = true;
+            }          
+        }       
+        if (Input.GetKeyDown(KeyCode.G))
+        {           
+            _canTurn = false;
+            _flashlight.enabled = false;
+            _turning = false;
+            _timesTurnig--;
+            _energyBar.SetActive(false);
+        }
+    }
 }
