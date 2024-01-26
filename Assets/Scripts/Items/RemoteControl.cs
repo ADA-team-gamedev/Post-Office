9dca376e29acfdd38dc6a0a0401b706cb12f2384
Dan-WarrioR <NorkinDanya2006@gmail.com>
2024-01-26 16:32:37 +0100

    Created remote control prefabs with all needed scripts

--- Assets/Scripts/Items/RemoteControl.cs
+++ Assets/Scripts/Items/RemoteControl.cs
@@ -9,9 +9,11 @@ public class RemoteControl : MonoBehaviour, IPickable, IUsable
 	public Action OnPickUpItem { get; set; }
 	public Action OnDropItem { get; set; }
 
+	[SerializeField] private Camera _playerCamera;
+
 	public void Use()
 	{
-		if (Physics.Raycast(transform.position, transform.forward, out RaycastHit hit))
+		if (Physics.Raycast(_playerCamera.transform.position, _playerCamera.transform.forward, out RaycastHit hit))
 		{
 			if (hit.transform.parent && hit.transform.parent.TryGetComponent(out GarageDoor garageDoor)) //transform.parent.TryGetComponent() - because garage door script lying on object without collider
 				garageDoor.InteractRemotely();
