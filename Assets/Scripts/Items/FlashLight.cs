c36a2a8403f5224c3a976001cff95bcde1e95c77
DanWarrioR <NorkinDanya2006@gmail.com>
2024-02-21 10:08:31 +0100

    Deleted unusable part of Flashlight script

--- Assets/Scripts/Items/FlashLight.cs
+++ Assets/Scripts/Items/FlashLight.cs
@@ -6,87 +6,34 @@ public class FlashLight : Item, IUsable
     [SerializeField] private Light _flashlight;
 
     [SerializeField] private bool _disableOnStart = true;
-    //[SerializeField] private Slider _batteryEnergyBar;
-    //[SerializeField] private GameObject _energyBar;
-
-    //[Header("Values")]
-    //[SerializeField] private int _numberOfActivations = 0;
-    //[SerializeField][Range(1, 100)] private float _dischargeRate;
-
-    //private bool _isCanTurnOn = false;
 
     public bool IsWorking { get; private set; } = false;
 
-    //private bool _charged = true;
-
 	private void Awake()
 	{
-        //OnPickUpItem += PickUpItem;
-
         IsWorking = !_disableOnStart;
 
         _flashlight.enabled = IsWorking;
 	}
 
-	#region Pickable
-
-	public void PickUpItem()
-    {
-        //_isCanTurnOn = true;
-    }
-
-    #endregion
-
     public void Use()
     {
-        //if (_isCanTurnOn && _charged && _numberOfActivations == 0)
-        //    TurnOn();
-        //else if (_numberOfActivations != 0)
-        //    TurnOff();
-
         if (!IsWorking)
             TurnOn();
         else
             TurnOff();
     }  
 
-    void Update()
-    {
-        //FlashLightWorking();
-    }
-
-    private void FlashLightWorking()
-    {
-        //if (_batteryEnergyBar.value <= 0)
-        //{
-        //    _flashlight.enabled = false;
-
-        //    _charged = false;
-        //}
-
-        //if (_isWorking && _charged)        
-        //    _batteryEnergyBar.value -= Time.deltaTime / _dischargeRate;        
-    }
-
     private void TurnOff()
     {                    
-        //_numberOfActivations--;
-
         _flashlight.enabled = false;
 
         IsWorking = false;       
-
-        //_energyBar.SetActive(false);
-        
     }
     private void TurnOn()
     {
-        //_numberOfActivations++;
-
         _flashlight.enabled = true;
 
         IsWorking = true;          
-
-        //_energyBar.SetActive(true);
     }
 }
